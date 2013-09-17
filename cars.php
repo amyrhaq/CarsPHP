2b9bbb361ed3db074971fcfa780451293c32155e
Your Name Here <amyr@amyrhaq.com>
2013-09-16 18:17:50 -0700

    Added PHP

--- cars.php
+++ cars.php
@@ -4,7 +4,14 @@
 <body>
 
 <?php
-
+$txt1="My car is swedish";
+$txt2="the middle console";
+$cars=array("Saab","BMW","Toyota");
+echo $txt1;
+echo "<br>";
+echo "The ignition is located in: $txt2";
+echo "<br>";
+echo "My car is a: {$cars[0]}";
 ?>
 
 </body>
